5c99bad09be07cef3050f52d7e3b89dc600cc659
zeni-lucas <zeni.lucas@gmail.com>
2024-09-12 16:53:38 -0300

    Criacao controller evento

--- src/main/java/com/inova_evento/app/controllers/EventosController.java
+++ src/main/java/com/inova_evento/app/controllers/EventosController.java
@@ -1,9 +1,62 @@
 package com.inova_evento.app.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.inova_evento.app.entities.EventosEntity;
+import com.inova_evento.app.services.EventosService;
+import com.inova_evento.app.services.UsuariosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/eventos")
 public class EventosController {
+
+    @Autowired
+    private EventosService eventosService;
+
+    @PostMapping("/create/{Id}")
+    public ResponseEntity<EventosEntity> create(@RequestBody EventosEntity evento, @PathVariable Long userId) {
+        try {
+            EventosEntity savedEvento = eventosService.save(evento, userId);
+            return new ResponseEntity<>(savedEvento, HttpStatus.CREATED);
+        } catch (SecurityException e) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<EventosEntity> update(@RequestBody EventosEntity evento, @PathVariable Long id) {
+        try {
+            EventosEntity updatedEvento = eventosService.update(evento, id);
+            return new ResponseEntity<>(updatedEvento, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/findbyid/{id}")
+    public ResponseEntity<EventosEntity> findById(@PathVariable Long id) {
+        try {
+            EventosEntity evento = eventosService.findById(id);
+            return new ResponseEntity<>(evento, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @GetMapping("/findall")
+    public ResponseEntity<List<EventosEntity>> findAll() {
+        try {
+            List<EventosEntity> eventos = eventosService.findALl();
+            return new ResponseEntity<>(eventos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
